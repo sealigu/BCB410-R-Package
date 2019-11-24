be2596e6abe76d36a6168d770d91791e79d78978
Shiyun Tang <shiyun.tang@mail.utoronto.ca>
2019-11-24 18:22:05 -0500

    n

--- R/runUA.R
+++ R/runUA.R
@@ -1,15 +1,15 @@
 #' Launch the shiny app for package UbiquitinAnalysis
-#' 
+#'
 #' A function that launches the shiny app for the package
 #' The code has been placed in \code{./inst/shiny-scripts}.
-#' 
+#'
 #' @return No return value but open up a shiny page.
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' runUA()
 #' }
-#' 
+#' @export
 #' @importFrom shiny runApp
 
 runUA <- function() {
