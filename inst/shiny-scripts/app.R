9f8d81f367cc387c8725cc47a4a603ba442c9271
Shiyun Tang <shiyun.tang@mail.utoronto.ca>
2019-12-02 00:27:58 -0500

    shiny

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -1,15 +1,36 @@
 library(shiny)
 
 ui <- fluidPange(
-  sliderInput(inputId = "num",
-              label = "Choose a number",
-              value = 25, min = 1, max = 100),
-  plotOutput("hist")
+  titlePanel("UbiquitinAnalysis: R package for Plotting Protein Relationship"),
+
+  sidebarPanel(
+    textInput(inputId = "UniProt_ID",
+              label = h3("Input an UniProt ID"),
+              placeholder = NULL),
+    textInput(inputId = "UniProt_ID",
+              label = h3("Input an UniProt ID"),
+              placeholder = NULL)
+  ),
+
+  mainPanel(
+    plotOutput("plot1"),
+    plotOutput("plot2"),
+    textOutput("percentage")
+  )
 )
 
 server <- function(input, output) {
-  output$hist <- renderPlot({
-    hist(rnorm(input$num))
+  plot1 = PlotProteinInteractions()
+  plot2 = PlotResModification()
+  percentage = GetSimilarPercentage()
+  output$plot1 <- renderPlot({
+    plot1
+  })
+  output$plot2 <- renderPlot({
+    plot2
+  })
+  output$percentage <- renderText({
+    paste("Similar Percentage: ", percentage)
   })
 }
 
